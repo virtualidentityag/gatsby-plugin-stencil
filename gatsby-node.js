b8431e8b25a2c79377b10dbf7e5f8554d3fd6321
Timo Mayer <timo.mayer@virtual-identity.com>
2020-12-03 07:51:21 +0100

    feat: stop on error, print error

--- gatsby-node.js
+++ gatsby-node.js
@@ -6,6 +6,9 @@ const fs = require("fs");
 */
 exports.onPostBuild = async ({}, pluginOptions) => {
   const stencil = require(pluginOptions.module + "/hydrate");
+
+console.log(pluginOptions.module);
+
   const files = glob.sync("public/**/*.html");
   const renderToStringOptions = pluginOptions.renderToStringOptions
     ? pluginOptions.renderToStringOptions
@@ -19,6 +22,18 @@ exports.onPostBuild = async ({}, pluginOptions) => {
           html,
           renderToStringOptions
         );
+
+        if (result.html === null) {
+
+          if (result.diagnostics) {
+            throw new Error(`${result.diagnostics[0].header}: ${result.diagnostics[0].messageText}`);
+          } else {
+            throw new Error(
+              "An unexpected error occured whilst executing stencil.renderToString()"
+            );
+          }
+        }
+
         fs.writeFileSync(file, result.html);
         return result;
       } catch (e) {
